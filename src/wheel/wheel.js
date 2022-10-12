3c701bf918eacaa45af05195e3397473a3964b05
AidanHanley <hanleya@etown.edu>
2022-10-12 10:53:14 -0400

    prompt randomizer added

---
+++ src/wheel/wheel.js
@@ -0,0 +1,48 @@
+
+
+    var prompt_list = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    var prompt_list_b = ["i", "j", "k", "l"];
+
+    var u_prompts = [];
+    var u_prompts_b = [];
+
+    /**
+     * Assigns prompts to players at the beginning of a new round.
+     * @param {int} n - number of players in lobby
+     * @param {boolean} d - if round is a double round
+     * @param {boolean} b - if game is using two lists
+     */
+    function assignPrompts(n, d, b) {
+        u_prompts = [];
+        u_prompts_b = [];
+
+        if (prompt_list.length < (d ? 2 * n : n) || (b && prompt_list_b.length < n )) {
+            console.warn("too few prompts");
+            return false;
+        }
+
+        for (let i = 0; i < n; i++) {
+            u_prompts.push(...prompt_list.splice(randBelow(prompt_list.length), 1));
+            console.warn(u_prompts[u_prompts.length - 1]);
+
+            if (d) {
+                u_prompts_b.push(...prompt_list.splice(randBelow(prompt_list.length), 1));
+                console.warn(u_prompts_b[u_prompts_b.length - 1]);
+            } else if (b) {
+                u_prompts_b.push(...prompt_list_b.splice(randBelow(prompt_list_b.length), 1));
+                console.warn(u_prompts_b[u_prompts_b.length - 1]);
+            }
+
+        }
+
+        return true;
+    }
+
+    /**
+     * Generates a random number between 0 (inclusive) and n (exclusive)
+     * @param {int} n - The upper bound for the random number
+     */
+    function randBelow(n) {
+        return Math.floor(Math.random() * n);
+    }
+
