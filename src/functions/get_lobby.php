c16133bc28ac454b3d4e5dddf69f0cfdc3b28c38
AidanHanley <hanleya@etown.edu>
2022-10-31 11:29:34 -0400

    lobby.php displays players in lobby

--- src/functions/get_lobby.php
+++ src/functions/get_lobby.php
@@ -1,5 +1,22 @@
 <?php
     require "db_functions.php";
 
+    header("Content-Type: application/json; charset=utf-8");
 
+    if (isset($_GET["lobby"])) {
+        $lid = $_GET["lobby"];
+        $players = lobby_players($lid);
+    
+        if ($players) {
+            echo json_encode($players);
+        } else {
+            header("HTTP/1.1 400 Invalid Request"); 
+            echo `Lobby {$lid} doesn't exist`; 
+        }
+        
+    } else {
+        header("HTTP/1.1 400 Invalid Request"); 
+        echo "Missing required parameter!"; 
+    }
+    
 ?>
--- src/lobby/lobby.js
+++ src/lobby/lobby.js
@@ -1,6 +1,8 @@
 (function(){
     "use script";
 
+    const BASE_URL = "http://localhost/wheel_game/functions/"; //TEMPORARY: GIVE REAL URL LATER
+
     //~~~~~~~~~~~~~~~~~~~~~~~
     //  INITIALIZATION
     //~~~~~~~~~~~~~~~~~~~~~~~
@@ -8,7 +10,24 @@
     window.addEventListener('load', init);
     
     function init() {
-        console.warn(lobby);
+        get_players();
+    }
+    
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~
+    //  UPDATE FUNCTIONS
+    //~~~~~~~~~~~~~~~~~~~~~~~~
+
+    function update_players(players) {
+        let p_div = document.getElementById("players");
+
+        p_div.innerHTML = "";
+
+        players.forEach((p) => {
+            let par = document.createElement("p");
+            par.innerHTML = p["playerName"];
+            p_div.appendChild(par);
+        });
     }
 
 
@@ -16,7 +35,21 @@
     //  DB QUERIES
     //~~~~~~~~~~~~~~~~~~~~~~~~
 
+    function get_players() {
+        let url = "functions/get_lobby.php?lobby=" + lobby;
 
+        fetch(url)
+            .then(checkStatus)
+            .then(update_players);
+    }
+
+    function checkStatus(response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(new Error(response.status + ": " + response.statusText));
+        }
+    }
 
 
 })();
