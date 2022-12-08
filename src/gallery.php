95d583a726a47660195d348126e64916d0a9056e
AidanHanley <hanleya@etown.edu>
2022-12-08 14:53:25 -0500

    cosmetic

--- src/gallery.php
+++ src/gallery.php
@@ -16,6 +16,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="gallery/gallery.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">    
     <script> lobby = "<?php echo $lobby;?>";</script>
     <script> player = "<?php echo $player;?>";</script>
     <script src="gallery/gallery.js"></script>
@@ -24,7 +26,9 @@
 <body>
     <h1 id="prompt"></h1>
     <h2 id="player"></h2>
-    <img id="image">
+    <div>
+        <img id="image">
+    </div>
     <button id="next-btn">Next</button>
 </body>
 </html>
