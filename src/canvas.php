5c870b0b67d8543fb534ddc5580c87d363d16450
AidanHanley <hanleya@etown.edu>
2022-12-08 14:53:22 -0500

    cosmetic

--- src/canvas.php
+++ src/canvas.php
@@ -19,7 +19,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="canvas/canvas.css"></link>
+    <link rel="stylesheet" href="canvas/canvas2.css"></link>
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">    
     <script> lobby = "<?php echo $lobby;?>";</script>
     <script> player = "<?php echo $player;?>";</script>
     <script> prompt = "<?php echo $prompt;?>";</script>
